d7dbb728cdc691c1a20b81564ee67bd43ee0a415
shzirley <shzirley@gmail.com>
2026-01-31 20:36:05 +0700

    fix init

--- ai/__init__.py
+++ ai/__init__.py
@@ -1,5 +1,6 @@
 from .model import ClaritasModel
 from .config import ModelConfig
+from .LLM import generate_clinical_report
 
 __version__ = "1.0.0"
-__all__ = ["ClaritasModel", "ModelConfig"]
+__all__ = ["ClaritasModel", "ModelConfig","generate_clinical_report"]
